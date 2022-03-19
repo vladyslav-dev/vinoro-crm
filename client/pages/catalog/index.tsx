5bd27d2306f8bb4c465ef3f61ca4aa234f589fdb
Vladyslav <51026693+vladyslav-dev@users.noreply.github.com>
2022-03-19 19:11:15 +0200

    Fixed 404 page

--- client/pages/catalog/index.tsx
+++ client/pages/catalog/index.tsx
@@ -4,31 +4,31 @@ import CatalogService from '@/services/CatalogService';
 import { NextPage, GetServerSideProps } from 'next';
 import { ICatalog } from '@/interfaces/catalog';
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+// export const getServerSideProps: GetServerSideProps = async (context) => {
 
-    const catalog = await CatalogService.createCatalog({
-        catalog_name: {
-            ru: '',
-            uk: '',
-            en: '',
-        },
-        catalog_image: '',
-        visibility: true
-    })
+//     const catalog = await CatalogService.createCatalog({
+//         catalog_name: {
+//             ru: '',
+//             uk: '',
+//             en: '',
+//         },
+//         catalog_image: '',
+//         visibility: true
+//     })
 
-    return {
-        props: {
-            catalog
-        },
-      }
-}
+//     return {
+//         props: {
+//             catalog
+//         },
+//       }
+// }
 
 interface CatalogProps {
-    catalog: ICatalog;
+    catalog?: ICatalog;
 }
 
 const Catalog: NextPage<CatalogProps> = ({ catalog }) => {
-    console.log('catalog', catalog)
+
     return (
         <div>
             Catalog
